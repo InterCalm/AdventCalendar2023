a6db201a3d1ea95bcbb0bce7e05f3e35c2588ed3
InterCalm <16158481+InterCalm@users.noreply.github.com>
2023-12-01 22:44:59 -0800

    update day1 to include outline of part2
    fixes typo and add outline for p2

--- Day1_Trebuchet.py
+++ Day1_Trebuchet.py
@@ -1,25 +1,36 @@
-#prompt AventCalendar 2023
+#prompt AdventCalendar 2023
 #Decode a given text file where a hidden number is the first and last digit in each line
 #calculate the sum
 
 def exp_result():
     #get Document split per return
-    file = open("input.txt", "r")
+    file = open("AdventCalendar2023/input.txt", "r")
     Counter = 0
     # Reading from file
     Content = file.read()
     contentList = Content.split("\n")
     sum = 0
+    altsum = 0
     for i in contentList:
         if i:
             Counter += 1
             exp = decoder(i)
             sum = sum + exp
 
+            value = read_decoder(i)
+            newexp = decoder(value)
+            altsum = altsum + newexp
+
+
+    #print result1
     strCounter = str(Counter)
     strSum = str(sum)
     print("This is the number of lines in the file: "+strCounter)
-    print("this is the total: " + strSum)
+    print("this is the first total: " + strSum)
+
+    # print result2
+    strAltSum = str(altsum)
+    print("this is the second total: " + strAltSum)
 
 
 def decoder(string):
@@ -33,7 +44,6 @@ def decoder(string):
             numbers = numbers+" "+i
 
     print("hidden Numbers: " + numbers)
-
     #get first and last numbers
     separate = numbers.split()
     firstint = separate[0]
@@ -44,16 +54,32 @@ def decoder(string):
     return intresult
 
 
-######################################################################################################################
 #part2, Read written numbers in the coded message and take the first and last digit and sum
+def read_decoder(string):
+    converter = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    #Take string read string for numbers and convert the text into a number and run in decode function
+    print("Read string Input message was :" + string)
+    for i in string:
+        string = string.replace("one", "1")
+        string = string.replace("two", "2")
+        string = string.replace("three", "3")
+        string = string.replace("four", "4")
+        string = string.replace("five", "5")
+        string = string.replace("six", "6")
+        string = string.replace("seven", "7")
+        string = string.replace("eight", "8")
+        string = string.replace("nine", "9")
+    print(string)
 
+    return string
 
 
 
 #Run Functions
 #enCodeStr = input("Please pass the encoded str:")
 #decoder(enCodeStr)
+#read_decoder(enCodeStr)
+#decoder(read_decoder(enCodeStr))
 
 exp_result()
-
 
